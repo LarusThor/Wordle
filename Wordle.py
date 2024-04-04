dbeee557840a0a66bd44f7adbfd3c7f6a63e1ec4
LarusThor <142886101+LarusThor@users.noreply.github.com>
2024-04-04 13:49:11 +0000

    update

--- Wordle.py
+++ Wordle.py
@@ -1,22 +1,30 @@
 from random import *
+from BST import *
 
 class Wordle:
     def __init__(self) -> None:
         pass
 
-    def display_word(self, word):
-        return word
-    
+    def display_word(self):
+        word = choice(self.word_bank_reader())
+        secret_word = BSTMap()
+        letter_counter = 0
+        for key in word:
+            secret_word.insert(key, letter_counter)
+            letter_counter += 1
+        return secret_word
+
+
     def word_bank_reader(self):
         word_list = []
         with open('wordbank.csv', newline='') as wordbank:
             for i in wordbank:
+                i = i.rstrip()
                 word_list.append(i)
-        word = choice(word_list)
-        return word
+        return word_list
 
 wordle = Wordle()
 
-print(wordle.word_bank_reader())
+print(wordle.display_word())
     
 
