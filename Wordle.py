0504cc80ad8fe10d4d359cd61d362c4de8c04477
steinarorn <steinar98@gmail.com>
2024-04-06 11:43:58 +0000

    f

--- Wordle.py
+++ Wordle.py
@@ -1,12 +1,20 @@
 from random import *
+class Word:
+    def __init__(self, no_guesses, word_length) -> None:
+        secret_word = choice(self.word_bank_reader(word_length))
+        self.secret_word_dict = self.word_into_dict(secret_word)
+        self.word_length = word_length
+        self.no_guesses = no_guesses
 
-
-class Wordle:
-    def __init__(self) -> None:
-        self.secret_word_dict = self.word_into_dict(choice(self.word_bank_reader()))
-        self.word_length = len(self.secret_word_dict)
-        self.num_guesses = self.word_length
-
+    def word_bank_reader(self, word_length):
+        word_list = []
+        with open('wordbank.csv', newline='') as wordbank:
+            for i in wordbank:
+                i = i.rstrip()
+                if len(i) == word_length:
+                    word_list.append(i)
+        return word_list
+    
     def word_into_dict(self, word):
         secret_word_dict = {}
         index = 0
@@ -16,6 +24,10 @@ class Wordle:
 
         return secret_word_dict
 
+class Wordle:
+    def __init__(self) -> None:
+        self.word = None
+        
     def format_guess_str(self, word):
         ret_str = ""
         for letter in word:
@@ -23,30 +35,16 @@ class Wordle:
         return ret_str
     
     def valid_guess(self, guess):
-        if len(guess) == self.word_length and guess.isalpha():
+        if len(guess) == self.word.word_length and guess.isalpha():
             return True
         return False
-
-    def word_bank_reader(self):
-        word_list = []
-        with open('wordbank.csv', newline='') as wordbank:
-            for i in wordbank:
-                i = i.rstrip()
-                word_list.append(i)
-        return word_list
-    
-    def format_guess_str(self, word):
-        ret_str = ""
-        for letter in word:
-            ret_str += f"{letter} "
-        return ret_str
     
     def check_guess(self, word):
-        guess_word = self.word_into_dict(word)
+        guess_word = self.word.word_into_dict(word)
         ret_str = ""
         for index, letter in guess_word.items():    
-            if letter in self.secret_word_dict.values():
-                if self.secret_word_dict[index] == letter:
+            if letter in self.word.secret_word_dict.values():
+                if self.word.secret_word_dict[index] == letter:
                     ret_str += "C "   
                 else:
                     ret_str += "c "      
@@ -56,35 +54,45 @@ class Wordle:
         return ret_str
 
     def play_game(self):
-        print("Take a guess!")
-        guess_word = input().lower()
+        
+        game_input = input("Start new game? (Y/N): ").lower()
+        while game_input == "y":
+            no_guesses = int(input("Number of guesses: "))
+            word_length = int(input("Input word length: "))
+            self.play_round(no_guesses, word_length)
+            game_input = input("Start new game? (Y/N): ").lower()
+
+
+    def play_round(self, no_guesses, word_length):
+        self.word = Word(no_guesses, word_length)
+        print(self.word.secret_word_dict)
         guess_counter = 0
-        while guess_counter < self.num_guesses:
+        while guess_counter < self.word.no_guesses:
+            print("Take a guess!")
+            guess_word = input().lower()
             validity_check = self.valid_guess(guess_word)
             if validity_check:
+                guess_counter += 1
                 guess = self.check_guess(guess_word)
                 print()
                 print(self.format_guess_str(guess_word))
                 print(guess)
-                if guess == "C "* self.word_length:
-                    guess_counter += 1
-                    print("You won!")
+                if guess == "C "* self.word.word_length:
+                    print("You won!") 
                     return
-                else:
-                    print("Take a guess!")
-                    guess_word = input().lower()
-                    guess_counter += 1
             else:
-                print("Enter valid guess")
-                guess_word = input().lower()
+                print()
+                print(f"Not a valid guess, word must contain {word_length} letters")
+                print()
+                
         print("You lost!")
         return
+        
+
 
 wordle = Wordle()
-print(wordle.secret_word_dict)
-print(wordle.check_guess("mrush"))
 
-
+wordle.play_game()
 
     
 
